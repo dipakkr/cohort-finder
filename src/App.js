56e6c36b6e861f11e2b30545b5af31ab893bc3cd
Deepak <deepak.kumar@exattosoft.com>
2021-02-09 01:04:10 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import tailwindlogo from './tailwind-css-logo.svg';
+import reactlogo from './react-logo.png';
+import { SectionA } from './components/SectionA';
+
+const App = () => {
+  return (
+    <div className="">
+      
+      <SectionA/>
+
+    </div>
+  );
+}
+
+export default App;
