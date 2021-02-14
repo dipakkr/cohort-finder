2097c6fc82db283418362ee73b99b6898cd31a46
Deepak <deepak.kumar@exattosoft.com>
2021-02-15 00:57:19 +0530

    updated

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,7 +3,7 @@ export const Hero = () => {
     <section class="text-gray-400 bg-gray-900 body-font">
     <div class="container mx-auto flex px-5 py-10 md:flex-row flex-col flex-col-reverse	 items-center">
       <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mt-4 mb-16 md:mb-0 items-center text-center">
-        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Before they sold out
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Find the best cohort around you
           <br class="hidden lg:inline-block"/>readymade gluten
         </h1>
         <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
